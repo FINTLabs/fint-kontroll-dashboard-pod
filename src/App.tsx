c8de5c8ea8b161e2e4d6ceecd88d473586104e63
jennifer-fintlabs <jennifer@fintlabs.no>
2023-10-06 15:06:08 +0200

    show a test message

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
+import { MessageBus } from '@podium/browser';
+
 
 function App() {
+    const [username, setUsername] = useState<string>('');
+    const messageBus = new MessageBus();
+
+    messageBus.subscribe(
+        'testChannel',
+        'testTopic',
+        (event) => {
+            const user = event.payload;
+            setUsername(user as string);
+        }
+    )
+
   return (
     <div className="App">
       <header className="App-header">
         <p>
           Really cool Dashboard coming soon!
         </p>
+          <p>
+              {username}
+          </p>
       </header>
     </div>
   );
