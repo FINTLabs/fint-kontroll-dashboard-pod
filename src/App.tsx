bd53fe8b4a462a7563d76645eebfd1a47d2a08dd
jennifer-fintlabs <jennifer@fintlabs.no>
2023-10-06 16:37:44 +0200

    added test subscribe

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
+import { MessageBus } from '@podium/browser';
 
 function App() {
+    const messageBus = new MessageBus();
+    const [username, setUsername] = useState<string>('');
+
+    messageBus.subscribe(
+        'testChannel',
+        'testTopic',
+        (event) => {
+            const user = event.payload;
+            setUsername(user as string);
+        }
+    )
+
     return (
     <div className="App">
       <header className="App-header">
         <p>
           Really cool Dashboard coming soon!
         </p>
+          <p>
+              Test: {username}
+          </p>
       </header>
     </div>
   );
