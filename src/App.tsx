8e780bea184d3d9db5708a92eb8d7630e6816d2d
jennifer-fintlabs <jennifer@fintlabs.no>
2023-10-06 16:08:07 +0200

    added useEffect to subscribe

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,13 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import './App.css';
-import { MessageBus } from '@podium/browser';
-
 
 function App() {
-    const [username, setUsername] = useState<string>('');
-
-
-    useEffect(() => {
-        const messageBus = new MessageBus();
-        messageBus.subscribe(
-            'testChannel',
-            'testTopic',
-            (event) => {
-                console.log("inside message subscribe", event.payload);
-                const user = event.payload;
-                setUsername(user as string);
-            }
-        );
-
-    }, []);
-
     return (
     <div className="App">
       <header className="App-header">
         <p>
           Really cool Dashboard coming soon!
         </p>
-          <p>
-              {username}
-          </p>
       </header>
     </div>
   );
