16776bf82cbfd298b824fa161fa0b76d7941bfa4
Linda Loftsgarden <linda-fintlabs@users.noreply.github.com>
2023-11-17 13:14:24 +0100

    Changed dasboard text and color

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import './App.css';
-
+import SettingsSuggestTwoToneIcon from '@mui/icons-material/SettingsSuggestTwoTone';
 function App() {
 
     return (
     <div className="App">
       <header className="App-header">
         <p>
-          Really cool Dashboard coming soon!
+          Velkommen til FINT-Kontroll!
         </p>
+          <SettingsSuggestTwoToneIcon sx={{height: '3em', width: '3em'}}/>
       </header>
     </div>
   );
